4078184e31ea5a1381ec3d0c5b86dd301504813d
Albert Minnie <albert.minnie@gmail.com>
2023-03-13 16:03:27 +0200

    struggle to store the keys

--- app/src/main/java/com/example/chatmate/MainActivity.kt
+++ app/src/main/java/com/example/chatmate/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.chatmate
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Base64
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,9 +15,16 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatmate.databinding.ActivityMainBinding
+import com.example.chatmate.ui.login.LoginActivity
+import java.security.KeyStore
+import javax.crypto.Cipher
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        private const val ANDROID_KEYSTORE_PROVIDER = "AndroidKeyStore"
+        private const val ORGANIZATION_KEY_ALIAS = "organization_alias"
+        private const val API_KEY_ALIAS = "api_key_alias"
+    }
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
@@ -36,15 +45,18 @@ class MainActivity : AppCompatActivity() {
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
@@ -61,19 +73,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hasLoginCredentials(): Boolean {
-        val keyPairGenerator = KeyPairGenerator.getInstance(
-            KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore")
-        keyPairGenerator.initialize(
-            KeyGenParameterSpec.Builder(
-                "myKey",
-                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
-                .setKeySize(2048)
-                .build())
-        keyPairGenerator.generateKeyPair()
-
         val keyStore = KeyStore.getInstance("AndroidKeyStore")
         keyStore.load(null)
-        return keyStore.containsAlias("myKey")
+
+        val organizationAlias = "organization_alias"
+        val apiKeyAlias = "api_key_alias"
+
+        val organizationEntry = keyStore.getEntry(organizationAlias, null)// as KeyStore.SecretKeyEntry
+        val apiKeyEntry = keyStore.getEntry(apiKeyAlias, null)// as KeyStore.SecretKeyEntry
+        if (organizationEntry == null && apiKeyEntry == null)
+            return false
+
+        val organizationSecretKeyEntry: KeyStore.SecretKeyEntry = organizationEntry as KeyStore.SecretKeyEntry
+        val apiKeySecretKeyEntry: KeyStore.SecretKeyEntry = apiKeyEntry as KeyStore.SecretKeyEntry
+
+        val organizationCipher = Cipher.getInstance("AES/CBC/PKCS7Padding").apply {
+            init(Cipher.DECRYPT_MODE, organizationSecretKeyEntry.secretKey)
+        }
+        val apiKeyCipher = Cipher.getInstance("AES/CBC/PKCS7Padding").apply {
+            init(Cipher.DECRYPT_MODE, apiKeySecretKeyEntry.secretKey)
+        }
+
+        val sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        val organizationEncryptedString = sharedPreferences.getString(organizationAlias, null)
+        val apiKeyEncryptedString = sharedPreferences.getString(apiKeyAlias, null)
+
+        if (organizationEncryptedString != null && apiKeyEncryptedString != null) {
+            val organizationEncryptedBytes = Base64.decode(organizationEncryptedString, Base64.DEFAULT)
+            val apiKeyEncryptedBytes = Base64.decode(apiKeyEncryptedString, Base64.DEFAULT)
+
+            val organizationBytes = organizationCipher.doFinal(organizationEncryptedBytes)
+            val apiKeyBytes = apiKeyCipher.doFinal(apiKeyEncryptedBytes)
+
+            val organization = String(organizationBytes, Charsets.UTF_8)
+            val apiKey = String(apiKeyBytes, Charsets.UTF_8)
+
+            // Use the organization and apiKey here
+            println("****** organization = $organization")
+            println("****** apiKey = $apiKey")
+        } else {
+            // No organization and apiKey were found
+        }
+
+        return false
     }
 }
--- app/src/main/java/com/example/chatmate/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/chatmate/ui/login/LoginActivity.kt
@@ -1,20 +1,27 @@
 package com.example.chatmate.ui.login
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Base64
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.Toast
+import com.example.chatmate.MainActivity
 import com.example.chatmate.databinding.ActivityLoginBinding
-
-import com.example.chatmate.R
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,8 +34,8 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val username = binding.username
-        val password = binding.password
+        val organization = binding.organization
+        val apiKey = binding.apiKey
         val login = binding.login
         val loading = binding.loading
 
@@ -42,10 +49,10 @@ class LoginActivity : AppCompatActivity() {
             login.isEnabled = loginState.isDataValid
 
             if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
+                organization.error = getString(loginState.usernameError)
             }
             if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
+                apiKey.error = getString(loginState.passwordError)
             }
         })
 
@@ -57,7 +64,7 @@ class LoginActivity : AppCompatActivity() {
                 showLoginFailed(loginResult.error)
             }
             if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
+                updateUiWithUser(loginResult.success, organization.text.toString(), apiKey.text.toString())
             }
             setResult(Activity.RESULT_OK)
 
@@ -65,18 +72,18 @@ class LoginActivity : AppCompatActivity() {
             finish()
         })
 
-        username.afterTextChanged {
+        organization.afterTextChanged {
             loginViewModel.loginDataChanged(
-                username.text.toString(),
-                password.text.toString()
+                organization.text.toString(),
+                apiKey.text.toString()
             )
         }
 
-        password.apply {
+        apiKey.apply {
             afterTextChanged {
                 loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
+                    organization.text.toString(),
+                    apiKey.text.toString()
                 )
             }
 
@@ -84,8 +91,8 @@ class LoginActivity : AppCompatActivity() {
                 when (actionId) {
                     EditorInfo.IME_ACTION_DONE ->
                         loginViewModel.login(
-                            username.text.toString(),
-                            password.text.toString()
+                            organization.text.toString(),
+                            apiKey.text.toString()
                         )
                 }
                 false
@@ -93,20 +100,73 @@ class LoginActivity : AppCompatActivity() {
 
             login.setOnClickListener {
                 loading.visibility = View.VISIBLE
-                loginViewModel.login(username.text.toString(), password.text.toString())
+                loginViewModel.login(organization.text.toString(), apiKey.text.toString())
             }
         }
     }
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome)
-        val displayName = model.displayName
-        // TODO : initiate successful logged in experience
-        Toast.makeText(
-            applicationContext,
-            "$welcome $displayName",
-            Toast.LENGTH_LONG
-        ).show()
+    private fun updateUiWithUser(model: LoggedInUserView, organization: String, apiKey: String) {
+        // Store the organization and apiKey in the Android Keystore
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+
+        val organizationAlias = "organization_alias"
+        val apiKeyAlias = "api_key_alias"
+
+        val organizationKey = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            .apply {
+                init(
+                    KeyGenParameterSpec.Builder(
+                        organizationAlias,
+                        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                    )
+                        .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                        .setKeySize(256)
+                        .build()
+                )
+            }
+            .generateKey()
+
+        val apiKeyKey = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            .apply {
+                init(
+                    KeyGenParameterSpec.Builder(
+                        apiKeyAlias,
+                        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                    )
+                        .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                        .setKeySize(256)
+                        .build()
+                )
+            }
+            .generateKey()
+
+        val organizationCipher = Cipher.getInstance("AES/CBC/PKCS7Padding")
+            .apply {
+                init(Cipher.ENCRYPT_MODE, organizationKey)
+            }
+        val organizationEncryptedBytes = organizationCipher.doFinal(organization.toByteArray(Charsets.UTF_8))
+
+        val apiKeyCipher = Cipher.getInstance("AES/CBC/PKCS7Padding")
+            .apply {
+                init(Cipher.ENCRYPT_MODE, apiKeyKey)
+            }
+        val apiKeyEncryptedBytes = apiKeyCipher.doFinal(apiKey.toByteArray(Charsets.UTF_8))
+
+        val organizationEncryptedString = Base64.encodeToString(organizationEncryptedBytes, Base64.DEFAULT)
+        val apiKeyEncryptedString = Base64.encodeToString(apiKeyEncryptedBytes, Base64.DEFAULT)
+
+        val sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        sharedPreferences.edit()
+            .putString(organizationAlias, organizationEncryptedString)
+            .putString(apiKeyAlias, apiKeyEncryptedString)
+            .apply()
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun showLoginFailed(@StringRes errorString: Int) {
